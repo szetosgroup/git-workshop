86f972d4b8548f0f0129bef3acf83844285d2a51
Mathis <sveitser@gmail.com>
2012-08-25 16:08:45 +0800

    Configure git Globally

--- configure-git.sh
+++ configure-git.sh
@@ -1,4 +1,6 @@
+git config --list
 git config --global user.name "Mathis Antony"
 git config --global user.email "sveitser@gmail.com"
 git config --global color.status auto
 git config --global color.branch auto 
+git config --list
